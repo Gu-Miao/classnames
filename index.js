59323cf66500ddd56cb7f4c0e64ab2ad98c419c4
Gu-Miao <1450206741@qq.com>
2020-11-27 17:57:22 +0800

    finish main function

---
+++ index.js
@@ -0,0 +1,31 @@
+import isPlainObject from './utils/isPlainObject.js'
+
+export default function classNames() {
+  const classSet = new Set()
+  for (let className of arguments) {
+    if (!className && className !== 0) continue
+
+    if (typeof className === 'string') {
+      classSet.add(className.trim())
+    } else if (typeof className === 'number' && isFinite(className)) {
+      classSet.add(className.toString())
+    } else if (Array.isArray(className)) {
+      className.forEach(className => {
+        if (typeof className === 'string') {
+          classSet.add(className.trim())
+          return
+        }
+        if (typeof className === 'number' && isFinite(className)) {
+          classSet.add(className.toString())
+          return
+        }
+      })
+    } else if (isPlainObject(className)) {
+      Object.keys(className).forEach(key => {
+        if (className[key]) classSet.add(key.toString().trim())
+      })
+    }
+  }
+
+  return Array.from(classSet).join(' ')
+}
